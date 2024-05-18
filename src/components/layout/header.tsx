79f4abccd95d08e2aeb301063d77462de49efe16
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-05-18 13:46:54 -0400

    update data

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,14 @@
+import { useAuthStore } from "@/store/auth.store";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const setToken = useAuthStore((state) => state.setToken);
+
+  console.log(user);
   return (
     <header className="bg-gray-900 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -42,14 +49,36 @@ export const Header = () => {
                 Rent
               </a>
             </li>
-            <Link href={"/login"} className="hover:text-gray-400">
-              Sing In
-            </Link>
-            <li>
-              <Link href={"/register"} className="hover:text-gray-400">
-                Sing up
-              </Link>
-            </li>
+            {user ? (
+              <>
+                <li>
+                  <span>{user.name}</span>
+                </li>
+                <li
+                  onClick={() => {
+                    signOut();
+                    setUser(null);
+                    setToken("");
+                  }}
+                >
+                  Cerrar sesion
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href={"/login"} className="hover:text-gray-400">
+                    Sing In
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/register"} className="hover:text-gray-400">
+                    Sing up
+                  </Link>
+                </li>
+              </>
+            )}
+            <li></li>
           </ul>
         </nav>
       </div>
